da184483599e3786a8f3e47bbc81b72226d4c5de
YugantGotmare <yugantgotmare123@gmail.com>
2023-12-20 13:34:28 +0530

    Dashborad

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
-import streamlit as st 
-import yfinance as yf 
+import streamlit as st
+import yfinance as yf
 import pandas as pd
+import plotly.graph_objects as go
 
 st.title("Stock Market Dashboard")
 tickers = ('TSLA', 'AAP', 'MSFT', 'BTC-USD', 'ETH-USD', 'GOOGL', 'AMZN', 'AAPL', 'NFLX', 'NVDA')
 
 # Sidebar for user input
 st.sidebar.header("Settings")
-dropdown = st.sidebar.multiselect('Pick your assets', tickers)
+dropdown = st.sidebar.multiselect('Pick your assets', tickers, default=['GOOGL'])  # Set GOOGL as the default asset
 start = st.sidebar.date_input('Start date', value=pd.to_datetime('2023-01-01'))
 end = st.sidebar.date_input('End date', value=pd.to_datetime('today'))
 
-# Function to calculate cumulative returns
-def relativeret(df):
-    rel = df.pct_change()
-    cumret = (1 + rel).cumprod() - 1
-    cumret = cumret.fillna(0)
-    return cumret
-
 # Fetch and display data
 if len(dropdown) > 0:
-    df = relativeret(yf.download(dropdown, start=start, end=end)['Adj Close'])
-    st.header('{}'. format(dropdown))
-    st.line_chart(df)
+    df = yf.download(dropdown, start=start, end=end)['Adj Close']
+    df_candle = yf.download(dropdown, start=start, end=end)
+
+    # Display cumulative returns line chart
+    st.header('{}'.format(dropdown))
+    st.line_chart(df.pct_change().cumsum())
 
     # Display raw data in a table
     st.write("Raw Data:")
-    st.dataframe(yf.download(dropdown, start=start, end=end)['Adj Close'])
+    st.dataframe(df)
 
     # Display descriptive statistics
     st.write("Descriptive Statistics:")
@@ -42,3 +39,11 @@ if len(dropdown) > 0:
     st.markdown("""
     * Cumulative Return: The cumulative return is calculated based on the percentage change in the adjusted close prices.
     """)
+
+    # Plot candlestick chart at the bottom
+    fig = go.Figure(data=[go.Candlestick(x=df_candle.index,
+                                         open=df_candle['Open'],
+                                         high=df_candle['High'],
+                                         low=df_candle['Low'],
+                                         close=df_candle['Close'])])
+    st.plotly_chart(fig, use_container_width=True)
